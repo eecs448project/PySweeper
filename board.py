46eecd49308314a9f2d688a6f81ee82f292e2c6a
BenjaminWyss <b040w377@ku.edu>
2019-02-08 09:33:43 -0600

    wrote default constructors for Board and Cell classes

--- board.py
+++ board.py
@@ -3,14 +3,17 @@ class Board():
         """ Constructor for Board.
             What parameters do we want here?
         """
+        self.rows = 10
+        self.columns = 10
+        generateGrid()
         pass
-    
+
     def generateGrid(self):
         """ Generate grid
             Should we call this in __init__?
         """
         pass
-        
+
     def draw(self):
         """ Draw the grid on the surface.
         """
--- cell.py
+++ cell.py
@@ -2,4 +2,7 @@ class Cell():
     def __init__(self):
         """ Constructor for Cell class
         """
+        self.mine = False
+        self.revealed = True
+        self.flagged = False
         pass
