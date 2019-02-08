fab6aa089d291574290cfbbbb18bd7cd9c96bbc8
Jon Volden <volden.jon@gmail.com>
2019-02-08 10:36:05 -0600

    Update main program to use new gui.py class.

--- pysweeper.py
+++ pysweeper.py
@@ -6,7 +6,7 @@ from board import Board
 from cell import Cell
 from gui import GUI
 
-from constants import (ROWS, COLS, CELLWIDTH, CELLHEIGHT,
+from constants import (CELLWIDTH, CELLHEIGHT,
                       MARGIN, BORDER, COLOR)
 
 # PyGame Initializing
@@ -16,7 +16,7 @@ clock = pg.time.Clock()
 # Initialize screen/windows
 #@todo: Start the GUI here
 gameBoard = Board()
-screen = GUI(ROWS, COLS, CELLWIDTH, CELLHEIGHT, MARGIN, BORDER)
+screen = GUI(gameBoard)
 
 # Main game loop
 done = False
@@ -27,12 +27,16 @@ while not done:
             done = True
         #@todo: Add event handler here
         elif event.type == pg.MOUSEBUTTONDOWN:
+            gameBoard = Board(20, 20, 100)
+            screen = GUI(gameBoard)
             pass
 
     # Fill window and redraw toolbar and board.
     #@todo:
-    screen.window.fill(COLOR['WHITE'])
-    pass
+    screen.window.fill(COLOR['BLACK'])
+
+    screen.drawBoard(gameBoard)
+
     # Redraw entire window each frame. Good enough for Minesweeper.
     pg.display.flip()
     clock.tick(20)
