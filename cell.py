1acc9f4dbb2d9ae10cff59d1b451f5329f03df65
BenjaminWyss <b040w377@ku.edu>
2019-02-14 14:15:33 -0600

    Updated pydoc comments for Cell.py

--- cell.py
+++ cell.py
@@ -1,6 +1,14 @@
 class Cell():
+    """ The Cell class will be used to create the building blocks
+        of the game board.
+        It contains all information needed by a higher up board class
+        in order to implement the minesweeper game
+    """
     def __init__(self):
         """ Constructor for Cell class
+            Takes no parameters
+            sets each variable of the cell class to a default value.
+            Cells are constructed as nonrevealed, nonflagged, nonmines.
         """
         self.mine = False
         self.revealed = False
