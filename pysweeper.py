5efa06a61ac49cd2cfe0eebf9f10724321099c37
Saharsh Gupta <saharsh.gupta96@gmail.com>
2019-03-19 14:32:15 -0500

    Cleaned up unused variable.

--- pysweeper.py
+++ pysweeper.py
@@ -33,7 +33,7 @@ def main():
     pg.mixer.pre_init(44100, -16, 2, 512)
     pg.mixer.init()
     gameSound = Sound()
-    track=pg.mixer.music.load("resources/newback.wav") 
+    pg.mixer.music.load("resources/newback.wav") 
     pg.mixer.music.play()
     #These bools are used so that sounds don't play more than once per click.
     winBool = True
